f87bf0abf08912eaafde7028372291bc2ce3776e
KorriZokta <frederic.becker4@gmail.com>
2015-04-18 17:16:11 +0200

    My First Commit
    I'm sure it'is broken

---
+++ Assets/Scripts/Movement/MovementController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementController : MonoBehaviour {
+	public float PosX=1;
+	public float PosY=1;
+	public float PosZ=1;
+
+
+	void Start () {
+		PosX = transform.position.x;
+		PosY = transform.position.y;
+		PosZ = transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		PosX = transform.position.x;
+		PosY = transform.position.y;
+		PosZ = transform.position.z;
+		/*if (PosX < 5) 
+			transform.position = transform.position + new Vector3 (0.1f, 0, 0);
+		else
+			transform.position = transform.position + new Vector3 (-0.3f, 0, -0.2f);
+		if (PosZ < 5)
+			transform.position = transform.position + new Vector3 (0, 0, 0.1f);
+		else
+			transform.position = transform.position + new Vector3 (-0.2f, 0, -0.3f);*/
+		GetComponent<Rigidbody> ().AddTorque (new Vector3 (1, 1, 1));
+	}
+}
