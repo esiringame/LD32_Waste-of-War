0f3bcb51ef17e35cfe5e23fb8efc6ccbd25f49b9
ReMinoer <rmorillon@freneticexperience.com>
2015-04-18 15:28:30 +0200

    Add Singleton & Factory

---
+++ Assets/Scripts/DesignPattern/Factory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace DesignPattern
+{
+	public class Factory<T> : MonoBehaviour
+        where T : Factory<T>
+	{
+		public static T New()
+		{
+            var gameObject = new GameObject(typeof(T).ToString());
+			var component = gameObject.AddComponent<T>();
+			return component;
+		}
+		
+		public static T New(string prefabPath)
+		{
+			var gameObject = Instantiate(Resources.Load(prefabPath)) as GameObject;
+		    if (gameObject == null)
+		        throw new NullReferenceException();
+
+		    var component = gameObject.GetComponent<T>();
+		    if(component == null)
+		        Destroy(gameObject);
+
+		    return component;
+		}
+	}
+}
---
+++ Assets/Scripts/DesignPattern/Singleton.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace DesignPattern
+{
+
+	public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+	{
+		private static T _instance;
+		private static object _lock = new object();
+		private static bool applicationIsQuitting = false;
+
+		public static T Instance
+		{
+			get
+			{
+				if(applicationIsQuitting)
+				{
+					Debug.LogWarning("[Singleton] Instance '"+ typeof(T) +
+						"' already destroyed on application quit." +
+						" Won't create again - returning null.");
+					return null;
+				}
+				lock(_lock)
+				{
+					if(_instance == null)
+					{
+						_instance = (T) FindObjectOfType(typeof(T));
+						if(FindObjectsOfType(typeof(T)).Length > 1)
+						{
+							Debug.LogError("[Singleton] Something went really wrong " +
+								" - there should never be more than 1 singleton!" +
+								" Reopenning the scene might fix it.");
+							return _instance;
+						}
+						if(_instance == null)
+						{
+							GameObject singleton = new GameObject();
+							_instance = singleton.AddComponent<T>();
+							singleton.name = typeof(T).ToString();
+	 
+							DontDestroyOnLoad(singleton);
+	 					}
+					}
+					return _instance;
+				}
+			}
+		}
+
+		public void OnDestroy ()
+		{
+			if (!Application.isEditor)
+				applicationIsQuitting = true;
+		}
+
+	}
+
+}
