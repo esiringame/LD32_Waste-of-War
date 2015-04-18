ae06309f79109f4d56f363fe69bf80f20d02bf02
KorriZokta <frederic.becker4@gmail.com>
2015-04-19 01:25:15 +0200

    Handle player moves

--- Assets/Scripts/Movement/MovementController.cs
+++ Assets/Scripts/Movement/MovementController.cs
@@ -1,81 +1,71 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MovementController : MonoBehaviour {
-	public float PosX=1;
-	public float PosY=1;
-	public float PosZ=1;
-	private Vector3 east = Vector3.right;
-	private Vector3 north = Vector3.forward;
-	private Vector3 west = Vector3.left;
-	private Vector3 south = Vector3.back;
-	public Vector3 Direction;
-	public float TimeKeyPressed;
-	private float startTime;
-	public float NextCaseDistance = 0.1f;
-	private Vector3 startMarker;
-	private Vector3 endMarker;
+public class MovementController : MonoBehaviour
+{
+	public Vector2 PositionCase { get; private set; }
+	public float Speed = 1;
 
+	private static Vector3 east = Vector3.right;
+	private static Vector3 north = Vector3.up;
+	private static Vector3 west = Vector3.left;
+	private static Vector3 south = Vector3.down;
+	private const float CaseSize = 1;
 
-	void Start () {
-		PosX = transform.position.x;
-		PosY = transform.position.y;
-		PosZ = transform.position.z;
-		Direction = Vector3.zero;
-		TimeKeyPressed = Time.realtimeSinceStartup;
+	public Vector3 Direction { get; private set; }
+	public Vector3 Destination { get; private set; }
+
+	private float pressedTimeElapsed;
+	private const float PressedTimePeriod = 0.2f;
+
+	void Start ()
+	{
+		Direction = Vector3.right;
+		Destination = transform.position;
+
+		pressedTimeElapsed = 0;
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	void Update ()
+	{
 		float horizontalInput = Input.GetAxis ("Horizontal");
 		float verticalInput = Input.GetAxis ("Vertical");
-		PosX = transform.position.x;
-		PosY = transform.position.y;
-		PosZ = transform.position.z; 
-		/*switch(3*horizontalInput + verticalInput) {
-		case 5 :
-		case 3:
-		case 2:
-		case 1:
-		case 0:
-		case -1:
-		case -2 :
-		case -3:
-		case -5:
-		default :
-		}*/
-		//if (TimeKeyPressed + 0.5f < Time.realtimeSinceStartup) {
+
+		if (transform.position == Destination)
+		{
+			Vector3 newDirection = Vector3.zero;
+
 			if (horizontalInput > 0)
-				Direction = east;
+				newDirection = east;
 			else if (horizontalInput < 0)
-				Direction = west;
+				newDirection = west;
 			else if (verticalInput > 0)
-				Direction = north;
+				newDirection = north;
 			else if (verticalInput < 0)
-				Direction = south;
-			else
-				return;
-			if (Direction != transform.forward) {
-				transform.forward = Direction;
-			} else {
-				startTime = Time.time;
-				startMarker = transform.position;
-				endMarker = startMarker+NextCaseDistance*transform.forward;
-				//while(Time.time<0.5f)
-					GoForward ();
+				newDirection = south;
+
+			if (newDirection == Vector3.zero)
+				pressedTimeElapsed = 0;
+			if (Direction == newDirection)
+			{
+				pressedTimeElapsed += Time.deltaTime;
+				
+				if (pressedTimeElapsed >= PressedTimePeriod)
+					Destination += Direction * CaseSize;
 			}
-			TimeKeyPressed = Time.realtimeSinceStartup;
-		//}
+			else
+				pressedTimeElapsed = 0;
+			
+			if (newDirection != Vector3.zero)
+				Direction = newDirection;
+		}
 
-
-		/*if(horizontalInput!=0)
-		transform.forward=horizontalInput*transform.right;*/
-	}
-
-	void GoForward() {
-		transform.position = transform.position + NextCaseDistance * transform.forward;
-		/*float fracJourney = (Time.time-startTime)/0.5f;
-		transform.position=Vector3.Lerp(startMarker,endMarker,fracJourney);*/
-
+		if (transform.position != Destination)
+		{
+			if (Vector3.Dot(Direction, Destination - this.transform.position) > 0)
+				transform.position += Direction * Speed * Time.deltaTime;
+			else
+				transform.position = Destination;
+		}
 	}
 }
