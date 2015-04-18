1f6e10f1641fc2572dbd515f87e6ea43b838038c
Ewakhine <wenell@hotmail.fr>
2015-04-18 20:20:36 +0200

    Creation of ProtoRankings (empty) and link between this and ProtoMain. Add two music for MainMenu. Add Quit Button.

---
+++ Assets/Scripts/MainMenu/ChangeMusic.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeMusic : MonoBehaviour {
+
+	//public AudioClip level2music;
+
+	private AudioSource source;
+
+	void Awake () 
+	{
+		source = GetComponent<AudioSource> ();
+	}
+
+	void OnLevelWasLoaded (int level)
+	{
+		/*if (level == 2) 
+		{
+			source.clip = level2music;
+			source.Play();
+		}*/
+
+		if (level == 0)
+		{
+			GameObject BackgroundMusic = GameObject.FindWithTag("BackgroundMusic");
+			Destroy(BackgroundMusic);
+		}
+	}
+}
---
+++ Assets/Scripts/MainMenu/DontDestroyOnLoad.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DontDestroyOnLoad : MonoBehaviour {
+
+	void Awake () {
+		if (gameObject)
+		//if (Application.loadedLevel == 0) 
+			DontDestroyOnLoad (gameObject);
+	}
+}
