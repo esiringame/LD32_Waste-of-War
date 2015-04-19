4ab25c0e2a92bc86f8f024cb2d3220aaff49e8c2
Ewakhine <wenell@hotmail.fr>
2015-04-19 19:28:34 +0200

    Creation of ProtoManager.

--- Assets/Scripts/Camera/FollowCamera.cs
+++ Assets/Scripts/Camera/FollowCamera.cs
@@ -9,14 +9,14 @@ public class FollowCamera : MonoBehaviour {
 	private int width;
 
 	void Start () {
-		transform.position = new Vector3(player.transform.position.x, 5.5f, transform.position.z);
+		transform.position = new Vector3(8.0f, 5.5f, transform.position.z);
 	}
 
 	void Update () {
 		height = Grid.Instance.Height;
 		width = Grid.Instance.Width;
 
-		if (player.transform.position.x > 5 && player.transform.position.x < width - 5)
+		if (player.transform.position.x > 8 && player.transform.position.x < width - 8)
 			transform.position = new Vector3(player.transform.position.x, 5.5f, transform.position.z);
 	}
 }
