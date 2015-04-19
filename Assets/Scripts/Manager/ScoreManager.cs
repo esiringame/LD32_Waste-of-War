a4da544dc12c0f31f73e8827a2a46e026a9039ce
pouloup <paul.ledenn@wanadoo.fr>
2015-04-19 21:41:11 +0200

    fix ScoreManager + .meta

--- Assets/Scripts/Manager/ScoreManager.cs
+++ Assets/Scripts/Manager/ScoreManager.cs
@@ -17,8 +17,8 @@ public class ScoreManager : Singleton<ScoreManager> {
 }
 
 public class Score : IEquatable<Score> , IComparable<Score>{
-	public string name{ get; set; };
-	public TimeSpan score{ get; set; };
+	public string name{ get; set; }
+	public TimeSpan score{ get; set; }
 	
 	public int CompareTo(Score compareScore)
 	{
