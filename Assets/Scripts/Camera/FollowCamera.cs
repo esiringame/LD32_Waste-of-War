e30724a568797464ba84fa572c1b4e194e19cf23
Ewakhine <wenell@hotmail.fr>
2015-04-19 20:58:07 +0200

    Creation of ProtoUIPause.

--- Assets/Scripts/Camera/FollowCamera.cs
+++ Assets/Scripts/Camera/FollowCamera.cs
@@ -9,14 +9,14 @@ public class FollowCamera : MonoBehaviour {
 	private int width;
 
 	void Start () {
-		transform.position = new Vector3(8.0f, 5.5f, transform.position.z);
+		transform.position = new Vector3(7.0f, 5.0f, transform.position.z);
 	}
 
 	void Update () {
 		height = Grid.Instance.Height;
 		width = Grid.Instance.Width;
 
-		if (player.transform.position.x > 8 && player.transform.position.x < width - 8)
-			transform.position = new Vector3(player.transform.position.x, 5.5f, transform.position.z);
+		if (player.transform.position.x > 7 && player.transform.position.x < width - 7)
+			transform.position = new Vector3(player.transform.position.x, 5.0f, transform.position.z);
 	}
 }
